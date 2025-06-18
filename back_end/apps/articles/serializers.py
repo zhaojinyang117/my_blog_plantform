7858acdda48ccda3f62cc8b6b49345d48040de00
zhaojinyang117 <2450414312@stu.tjise.edu.cn>
2025-06-18 22:48:58 +0800

    fix: 修复作者缺少的avatar字段

--- back_end/apps/articles/serializers.py
+++ back_end/apps/articles/serializers.py
@@ -10,7 +10,7 @@ class AuthorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "username", "email"]
+        fields = ["id", "username", "email", "avatar"]
 
 
 class ArticleSerializer(serializers.ModelSerializer):
